1c51f66db169b43c2eb563f4577fff9dea68d6b9
KSH Siaan <68361900+KshSiaan@users.noreply.github.com>
2024-02-13 23:40:44 +0600

    Add files via upload

---
+++ session01/homepage/main.js
@@ -0,0 +1,4 @@
+const style =
+  "background-color: #131313; padding:48px; border-radius:10px; color: white; font-size: 1.5em; text-shadow: -1px -1px 0 rgba(251, 1, 252, 0.5),1px 1px 0 rgba(4, 251, 246, 0.5);";
+console.log("%c(っ＾▿＾)۶🍸🌟🍺٩(˘◡˘ ) ", style);
+console.log("Have a nice day Dev!");
